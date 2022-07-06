70079da15219767088238c9d88cfd6a507b66e96
Gowthamap <gowthamap55555@gmail.com>
2022-07-07 04:04:13 +0530

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
-import {Invoice } from "./classes/invoice.js"
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "shaun",
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spend", amount);
+    return amount;
+  },
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log("hello", person.name);
+};
+
+greetPerson(me);
+
+console.log(me);
+
+import { Invoice } from "./classes/invoice.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
