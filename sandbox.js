c687d0f354a10117436a48f3e7728ce5d06c5f26
Gowthamap <gowthamap55555@gmail.com>
2022-07-07 00:00:57 +0530

    Type basics

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = "mario";
+var age = 20;
+var isBlackBelt = false;
+character = "luigi";
+age = 40;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let character = "mario";
+let age = 20;
+let isBlackBelt = false;
+
+character = "luigi";
+
+age = 40;
+
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI
+}
+
+console.log(circ(7.5));
