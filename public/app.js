9078fc5fd08d789c46cf971c909e6ce6df41f854
Gowthamap <gowthamap55555@gmail.com>
2022-07-07 05:05:36 +0530

    Generics

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 // list template instance
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -19,5 +19,24 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    list.render(doc, type.value, 'end');
+    list.render(doc, type.value, "end");
 });
+// GENERICS
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: "yoshi", age: 40 });
+// let docTwo = addUID('hello')
+console.log(docOne.age);
+const docThree = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "shaun" },
+};
+const docFour = {
+    uid: 2,
+    resourceName: "shoppingList",
+    data: ["bread", "milk", "toilet roll"],
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payments.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
@@ -13,19 +12,50 @@ const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
 // list template instance
-const ul = document.querySelector('ul')!;
+const ul = document.querySelector("ul")!;
 const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
   let doc: HasFormatter;
-  if(type.value === "invoice") {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  list.render(doc, type.value, 'end');
-
+  list.render(doc, type.value, "end");
 });
+
+// GENERICS
+const addUID = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: "yoshi", age: 40 });
+// let docTwo = addUID('hello')
+
+console.log(docOne.age);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: "person",
+  data: { name: "shaun" },
+};
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: "shoppingList",
+  data: ["bread", "milk", "toilet roll"],
+};
+
+console.log(docThree, docFour);
